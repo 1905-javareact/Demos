803a0123c93c37c77020ee2ac047459e1872d6e4
Abatson <abatson94@gmail.com>
2019-05-24 14:26:52 -0400

    added deploy script

--- 1Week/music-api/src/middleware/cors-filter.middleware.ts
+++ 1Week/music-api/src/middleware/cors-filter.middleware.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from "express";
 
 export function corsFilter (req:Request,res:Response,next){
+    console.log(req.headers.origin);
+    
+
     res.header('Access-Control-Allow-Origin', `${req.headers.origin}`)//this is a hack, if you do it in a project I will be very disappointed in you
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
     res.header('Access-Control-Allow-Credentials', 'true')
